f97499bb1d61c319696da4ee8a6ba87e4099e147
ne0-d <dhruvbhati2001@gmail.com>
2023-05-20 19:37:28 +0530

    continuing project updates

---
+++ frontend/src/components/Slider/sliderData.js
@@ -0,0 +1,22 @@
+const slider = [
+    {
+        title: "We cultivate new crops to make farmers increase profits!",
+        description: "'' Agricultural growth is a proven driver of poverty reduction. When agriculture stimulates growth ... the growth is twice as effective in reducing poverty as growth based in other sectors ''",
+        tag: "GIZA MDOE",
+        image: "https://images.unsplash.com/photo-1535379453347-1ffd615e2e08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80",
+    },
+    {
+        title: "Solutions for a healthier, sustainable future",
+        description: "'' Agriculture is the greatest and fundamentally the most important of our industries. The cities are but the branches of the tree of national life, the roots of which go deeply into the land. We all flourish or decline with the farmer. ''",
+        tag: "BERNARD BARUCH",
+        image: "https://plus.unsplash.com/premium_photo-1668456429053-f0eaeba5d435?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+    },
+    {
+        title: "Helping farmers.Fighting climate change.",
+        description: "'' I know of no pursuit in which more real and important services can be rendered to any country than by improving its agriculture, its breed of useful animals, and other branches of a husbandman's cares. ''",
+        tag: "GEORGE WASHINGTON",
+        image: "https://plus.unsplash.com/premium_photo-1661930182051-06673fc544f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80",
+    },
+];
+
+export default slider;
